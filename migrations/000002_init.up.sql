7b8becdd6b6753352ad15f12dabc0b6e5921a344
Yana <kuryshevamedia@mail.ru>
2021-11-24 02:05:10 +0300

    Fix error with null

---
+++ migrations/000002_init.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE dbo.t_purchases ALTER COLUMN ticket_num_economy_class SET DEFAULT 0;
+ALTER TABLE dbo.t_purchases ALTER COLUMN ticket_num_pr_economy_class SET DEFAULT 0;
+ALTER TABLE dbo.t_purchases ALTER COLUMN ticket_num_business_class SET DEFAULT 0;
+ALTER TABLE dbo.t_purchases ALTER COLUMN ticket_num_first_class SET DEFAULT 0;
