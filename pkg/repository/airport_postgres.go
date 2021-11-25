72c549bef9d0741e6f20530a8892fa3c85f7b7ab
Yana <kuryshevamedia@mail.ru>
2021-11-25 16:16:20 +0300

    Data insert - countries, aeroports; fix bug with aeroport repo

--- pkg/repository/airport_postgres.go
+++ pkg/repository/airport_postgres.go
@@ -44,7 +44,10 @@ func (r *AirportPostgres) Create(userId int, airport gvapi.Airport) (int, error)
 func (r *AirportPostgres) GetAll() ([]gvapi.Airport, error) {
 	var airports []gvapi.Airport
 
-	query := fmt.Sprintf("SELECT * FROM %s tl", airportTable)
+	query := fmt.Sprintf(`SELECT airport_id, airport_type, airport_name, airport_iso_country_id, `+
+		`COALESCE(airport_iata_code, '') as airport_iata_code, COALESCE(airport_home_link, '') as airport_home_link,`+
+		`COALESCE(airport_iso_region, '') as airport_iso_region, COALESCE(airport_municipality, '') as airport_municipality, `+
+		`visa_flg, quarantine_flg, covid_test_flg, lockdown_flg,change_dttm FROM %s`, airportTable)
 	err := r.db.Select(&airports, query)
 
 	return airports, err
@@ -53,7 +56,9 @@ func (r *AirportPostgres) GetAll() ([]gvapi.Airport, error) {
 func (r *AirportPostgres) GetById(airportId int) (gvapi.Airport, error) {
 	var airport gvapi.Airport
 
-	query := fmt.Sprintf(`SELECT * FROM %s 
+	query := fmt.Sprintf(`SELECT airport_id, airport_type, airport_name, airport_iso_country_id, airport_iso_region, airport_municipality, `+
+		`COALESCE(airport_iata_code, '') as airport_iata_code, COALESCE(airport_home_link, '') as airport_home_link, visa_flg, quarantine_flg, `+
+		`covid_test_flg, lockdown_flg,change_dttm FROM %s 
 	                      WHERE airport_id = $1`, airportTable)
 	if err := r.db.Get(&airport, query, airportId); err != nil {
 		switch err {
